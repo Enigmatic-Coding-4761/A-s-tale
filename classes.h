3e84cdd5751da18b4b53b9baa5d7feee1eedc96b
Alex Forrest <blazezombie27@gmail.com>
2024-02-15 00:40:37 +0000

    added some inventory logic

--- classes.h
+++ classes.h
@@ -6,8 +6,41 @@
 class player
 {
     int health;
-    std::array<int,5> inventory; 
-    
+    std::array<std::string,5> inventory; 
+    int arrows;
+    std::array<std::string,10> key_items;
+
+    void inventory_list()
+    {
+        std::cout << "backpack:";
+        for (int i = 0; i < 5; i++){
+            std::cout <<    i + 1 << inventory[i] << std::endl;
+        }
+        std::cout << "key items:";
+        for (int i = 0; i < 10; i++){
+            std::cout <<    i + 1 << key_items[i] << std::endl;
+        }
+    }
+
+    void inventory_add(std::string item)
+    {
+        for (int i = 0; i < 5;i++)
+        {
+            if (inventory[i] == ""){
+                inventory[i] = item;
+            }
+        }
+    }
+
+    void inventory_add_key(std::string item){
+        for (int i = 0; i < 10;i++)
+        {
+            if (key_items[i] == ""){
+                key_items[i] = item;
+            }
+        }
+    }
+
 };
 
 
