497a288ec2b42c80ee8fcee5cf475bfacca8216d
Alex Forrest <blazezombie27@gmail.com>
2024-02-15 15:08:43 +0000

    i'll contnue this later

---
+++ choices.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#ifndef choices
+#define choices
+class choice{
+    void choose(std::string choice1,std::string choice2,std::string choice3,std::string choice4)
+    {
+
+    }
+};
+
+
+
+
+#endif 
