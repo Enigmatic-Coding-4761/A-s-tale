c4b976fcde7821ae668e9f3f385d98b11e3959d3
Alex Forrest <blazezombie27@gmail.com>
2024-02-17 10:29:17 +0000

    finished choice logic

--- choices.h
+++ choices.h
@@ -3,9 +3,13 @@
 #ifndef choices
 #define choices
 class choice{
-    void choose(std::string choice1,std::string choice2,std::string choice3,std::string choice4)
+    int choose(std::string choice1,std::string choice2,std::string choice3,std::string choice4)
     {
-
+        int chosen;
+        std::cout << "1.    " <<choice1 << std::endl << "2.    " << choice2 << std::endl << "3.    " << choice3 << std::endl << "4.    " << choice4;
+        std::cout << "choose one:";
+        std::cin >> chosen;
+        return chosen;
     }
 };
 
