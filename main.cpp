c8ed5a4324ce9299822311896bf133b8c37f8821
Alex Forrest <blazezombie27@gmail.com>
2024-03-24 12:27:51 +0000

    cleaned up some things

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "choices.h"
 #include "classes.h"
 #include "functions.h"
 
@@ -7,8 +6,6 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << argv[1];
-    string name = argv[1];
-    player name;
     cout << "you wake up in a forrest" << endl <<  "(hint type 'inv' to view your inventory )";
+    
 }
