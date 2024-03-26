721659930b8f41c96e2b1024aa0aafe4c2498aa7
Alex Forrest <blazezombie27@gmail.com>
2024-03-26 20:26:56 +0000

    renamed some files, added some logic for the first choice

--- functions.h
+++ functions.h
@@ -1,12 +1,17 @@
 #include<iostream>
+#include <string>
+#include"player.h"
 #ifndef functions
 #define functions
+using namespace std;
+
 
 
 void press_to_continue(){
-    std::cout << std::endl << "press anything to continue: " ;
-    std::cin.ignore();
+  std::cout << std::endl << "press anything to continue: " ;
+  std::cin.ignore();
 }
+
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include "classes.h"
+#include <string>
+#include "player.h"
 #include "functions.h"
+#include "choices.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-    cout << "you wake up in a forrest" << endl <<  "(hint type 'inv' to view your inventory )";
+    cout << "you wake up in a forrest" << endl <<  "(hint type 'inv' to view your inventory)" << endl;
+    // choice_num is for the variable names, choicenum is for the function names
+    string choice_1 = choice1();
+
     
 }
