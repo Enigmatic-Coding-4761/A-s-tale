8f27031ff4b8a51d63ab3c339c7c87974c2419a0
alex forrest <blazezombie27@gmail.com>
2024-05-05 12:32:30 +0100

    added some story

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 #include "player.h"
 #include "functions.h"
 #include "choices.h"
@@ -9,8 +10,13 @@ using namespace std;
 int main()
 {
     cout << "you wake up in a forrest" << endl <<  "(hint type 'inv' to view your inventory)" << endl;
-    // choice_num is for the variable names, choicenum is for the function names
+    // choice_{num} is for the variable names, choice{num} is for the function names
     string choice_1 = choice1();
+    if (choice_1 == "a"){
+        cout << "You go deeper and find an abbandonned village" << "\n";
+        usleep(1000000);
+        cout << "You decide to explore some of the houses" << "\n";
+    }
 
     
 }
