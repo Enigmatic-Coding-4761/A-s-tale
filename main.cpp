8cd90c4cef237e45586294f1f6d33b4b43925274
alex forrest <blazezombie27@gmail.com>
2024-05-06 15:34:59 +0100

    added an ending, gitignore and started a credits func

--- main.cpp
+++ main.cpp
@@ -9,13 +9,30 @@ using namespace std;
 
 int main()
 {
+    player Maxwell;
+    string path; 
     cout << "you wake up in a forrest" << endl <<  "(hint type 'inv' to view your inventory)" << endl;
     // choice_{num} is for the variable names, choice{num} is for the function names
     string choice_1 = choice1();
     if (choice_1 == "a"){
-        cout << "You go deeper and find an abbandonned village" << "\n";
-        usleep(1000000);
-        cout << "You decide to explore some of the houses" << "\n";
+      cout << "You go deeper and find an abbandonned village" << "\n";
+      wait(2);
+      cout << "You decide to explore some of the houses" << "\n";
+      wait(2);
+      cout << "You explore the old market in search for food and shelter" << "\n";
+      wait(3);
+      cout << "You find a tin of \'Enigmatic Beanz\'" << "\n";
+      Maxwell.inventory_add("Enigmatic Beanz");
+      cout << "As you pick it up you notice a string attatched that opens a trapdoor" << "\n";
+      wait (1.5);
+      cout << "You fall down the trapdoor" << "\n";
+      wait(2.4);
+      cout << "You are trapped." << "\n";
+      wait(3);
+      system("clear");
+      cout << "GAME END" << "\n";
+      credits();
+      
     }
 
     
